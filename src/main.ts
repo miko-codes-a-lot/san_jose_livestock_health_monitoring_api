0ee48da7e56eb590702cfa5c0f9c563acd968e21
Miko Chu <miko.codecrafters@gmail.com>
2025-10-16 05:11:54 +0800

    fix main multiple app listen

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { SocketIoAdapter } from './_shared/adapters/socket-io.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
 
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
